28fea90a3a8ec74c3b4ba5beea614309357c1898
Carlos Robles <crm107@alu.ua.es>
2023-07-07 16:35:02 +0200

    Stream Beta 2

--- app/Http/Controllers/WebrtcStreamingController.php
+++ app/Http/Controllers/WebrtcStreamingController.php
@@ -12,12 +12,12 @@ class WebrtcStreamingController extends Controller
 
     public function index()
     {
-        return view('video-broadcast', ['type' => 'broadcaster', 'id' => Auth::id()]);
+        return view('broadcast', ['type' => 'broadcaster', 'id' => Auth::id()]);
     }
 
     public function consumer(Request $request, $streamId)
     {
-        return view('video-broadcast', ['type' => 'consumer', 'streamId' => $streamId, 'id' => Auth::id()]);
+        return view('broadcast', ['type' => 'consumer', 'streamId' => $streamId, 'id' => Auth::id()]);
     }
 
     public function makeStreamOffer(Request $request)
