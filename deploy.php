b99e8d4a8de35c05da65718dccb8441f40ab1c22
Carlos Robles <carlos@carlos.es>
2025-12-23 21:40:47 +0100

    update deploy

--- deploy.php
+++ deploy.php
@@ -5,55 +5,44 @@ require 'recipe/laravel.php';
 require 'contrib/php-fpm.php';
 require 'contrib/npm.php';
 
-// Project name
 set('application', 'Trollers');
-
-// Project repository
 set('repository', 'https://github.com/crm107-ua/Trollers.git');
 set('branch', 'main');
 
-// Indicamos la version de php-fpm
-set('php_fpm_version', '8.2');
+set('php_fpm_version', '8.3');
 
-// Shared files/dirs between deploys 
-add('shared_files', []);
-add('shared_dirs', ["public/images", "public/archivos"]);
+set('bin/php', '/usr/bin/php');
+set('bin/composer', '{{bin/php}} /usr/local/bin/composer');
 
-// Writable dirs by web server 
-add(
-    'writable_dirs',
-    [
-        "public/images/fotos",
-        "public/images/perfiles",
-        "public/images/mw3",
-        "public/images/stories",
-    ]
-);
+add('shared_dirs', ['public/images', 'public/archivos']);
 
-// Indicamos el usuario de despliegue en el servidor
-host('trollers.server')
+add('writable_dirs', [
+    'public/images/fotos',
+    'public/images/perfiles',
+    'public/images/mw3',
+    'public/images/stories',
+]);
+
+host('192.168.50.20')
     ->set('remote_user', 'deploy')
-    // ->set('port', '4851')
-    ->set('identity_file', '~/deploy')
+    ->set('identity_file', '/Users/carlosrobles/deploy')
     ->set('deploy_path', '/var/www/html/trollers');
 
-// Tareas del despliegue
 task('deploy', [
     'deploy:prepare',
     'deploy:shared',
     'deploy:vendors',
     'deploy:writable',
     'artisan:storage:link',
-    'artisan:view:cache',
-    'artisan:config:cache',
     'artisan:view:clear',
     'artisan:view:cache',
+    'artisan:config:cache',
     'deploy:composer_install',
-    'deploy:publish'
+    'deploy:publish',
 ]);
 
 task('deploy:composer_install', function () {
-    run('cd {{release_path}} && {{bin/composer}} update --optimize-autoloader --no-dev');
+    run('cd {{release_path}} && {{bin/composer}} install --no-dev --optimize-autoloader');
 });
 
 after('deploy:failed', 'deploy:unlock');
